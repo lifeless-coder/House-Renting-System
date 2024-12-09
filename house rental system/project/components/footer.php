d0751281802ede269c697fe51304abd5b6df9133
lifeless-coder <115774484+lifeless-coder@users.noreply.github.com>
2024-12-09 09:13:39 +0600

    Add files via upload

---
+++ house rental system/project/components/footer.php
@@ -0,0 +1,35 @@
+<!-- footer section starts  -->
+
+<footer class="footer">
+
+   <section class="flex">
+
+      <div class="box">
+         <a href="tel:1234567890"><i class="fas fa-phone"></i><span>123456789</span></a>
+         <a href="tel:1112223333"><i class="fas fa-phone"></i><span>1112223333</span></a>
+         <a href="#"><i class="fas fa-envelope"></i><span>houserent@gmail.com</span></a>
+         <a href="#"><i class="fas fa-map-marker-alt"></i><span>Dhaka, Bangladesh</span></a>
+      </div>
+
+      <div class="box">
+         <a href="home.php"><span>HOME</span></a>
+         <a href="about.php"><span>ABOUT</span></a>
+         <a href="contact.php"><span>CONTACT</span></a>
+         <a href="listings.php"><span>ALL-LISTING</span></a>
+         <a href="saved.php"><span>SAVED-PROPERTIES</span></a>
+      </div>
+
+      <div class="box">
+         <a href="#"><span>facebook</span><i class="fab fa-facebook-f"></i></a>
+         <a href="#"><span>twitter</span><i class="fab fa-twitter"></i></a>
+         <a href="#"><span>linkedin</span><i class="fab fa-linkedin"></i></a>
+         <a href="#"><span>instagram</span><i class="fab fa-instagram"></i></a>
+
+      </div>
+
+   </section>
+
+
+</footer>
+
+<!-- footer section ends -->
